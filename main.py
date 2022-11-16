8a89155cdf66e20a05d7bced7c809a0ce4aae923
silva <silva@example.com>
2022-11-16 19:55:17 +0100

    changed cooldowns and loaded mongodb connection from config file

--- main.py
+++ main.py
@@ -8,26 +8,32 @@ import pymongo
 import time
 import datetime
 
-#mongodb config
-client = pymongo.MongoClient("mongodb_connection_string")
+# open settings
+with open("./config.json") as config_file:
+    data = json.load(config_file)
+
+
+# mongodb config
+mongodb_connection = data["mongodb_connection"]
+client = pymongo.MongoClient(mongodb_connection)
 db = client.journalEntries
 
 print(db)
 
-#open settings
-with open("./config.json") as config_file:
-    data=json.load(config_file)
-
-#bot config
+# bot config
 token = data["token"]
 description = "hi im a bot"
-intents=discord.Intents.all()
-watchingActivity = discord.Activity(type=discord.ActivityType.watching, name="the universe unfold.")
+intents = discord.Intents.all()
+watchingActivity = discord.Activity(
+    type=discord.ActivityType.watching, name="the universe unfold.")
 
-#bot class
-bot = discord.Bot(command_prefix=commands.when_mentioned_or("$"), activity = watchingActivity, description=description, intents=intents)
+# bot class
+bot = discord.Bot(command_prefix=commands.when_mentioned_or(
+    "$"), activity=watchingActivity, description=description, intents=intents)
 
-#get quote function
+# get quote function
+
+
 def get_quote():
     response = requests.get('https://stoic-server.herokuapp.com/random')
     json_data = json.loads(response.text)
@@ -36,25 +42,29 @@ def get_quote():
 
     return quote
 
-#exec this when bot is executed
+# exec this when bot is executed
+
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is ready")
 
 
-#quote command
+# quote command
 @bot.slash_command(description="Get shown a random Stoic quote!")
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def quote(ctx):
     quote = get_quote()
     await ctx.respond(quote)
 
-#journal command
+# journal command
+
+
 @bot.slash_command(description="Add an entry to your journal")
-@commands.cooldown(1, 300, commands.BucketType.user)
+@commands.cooldown(1, 60, commands.BucketType.user)
 async def journal(ctx, message):
-    today = str(time.strftime("%d-%m-%Y")) 
-    #HOW TO INSERT INTO MONGODB
+    today = str(time.strftime("%d-%m-%Y"))
+    # HOW TO INSERT INTO MONGODB
     db.journalMessages.insert_one(
         {
             "message": message,
@@ -62,21 +72,20 @@ async def journal(ctx, message):
             "date": today,
         }
     )
-    #notify the user that their message was uploaded to the db
+    # notify the user that their message was uploaded to the db
     await ctx.respond('Your message has been journaled.', ephemeral=True)
 
 
-#myjournal command
+# myjournal command
 @bot.slash_command(description="Shows all your journal entries")
-@commands.cooldown(1, 300, commands.BucketType.user)
+@commands.cooldown(1, 60, commands.BucketType.user)
 async def myjournal(ctx):
-    #get users id
+    # get users id
     author = ctx.author.id
-    #this makes the db look for any key entry in author which the author id, basically a select * where author=authorid
+    # this makes the db look for any key entry in author which the author id, basically a select * where author=authorid
     key = {'author': author}
 
-
-    #because there might be more than 1 entry, for loop to print all messages
+    # because there might be more than 1 entry, for loop to print all messages
     responseMessages = []
     responseMessagesDate = []
     str = ""
@@ -86,14 +95,14 @@ async def myjournal(ctx):
 
     for d in db.journalMessages.find(key):
         responseMessagesDate.append(m['date'])
-    
+
     for msg in responseMessages:
         str += msg + '\n\n'
 
     for msgDate in responseMessagesDate:
         strDate += msgDate
-        
-    #print (m['message']) debugging
+
+    # print (m['message']) debugging
     if str != "":
         text1 = 'Your journal entries are:\n\n' + str
         await ctx.respond(text1, ephemeral=True)
@@ -102,39 +111,41 @@ async def myjournal(ctx):
         await ctx.respond(text2, ephemeral=True)
 
 
-#myjournaldate command
+# myjournaldate command
 @bot.slash_command(description="Date in dd-mm-YYYY, filter journal by date")
 @commands.cooldown(1, 300, commands.BucketType.user)
 async def myjournaldate(ctx, date):
     format = "%d-%m-%Y"
     try:
         datetime.datetime.strptime(date, format)
-        #get users id
+        # get users id
         author = ctx.author.id
-        #this makes the db look for any key entry in author which the author id, basically a select * where author=authorid
+        # this makes the db look for any key entry in author which the author id, basically a select * where author=authorid
         key = {
             'author': author,
             'date': date, }
 
-        #because there might be more than 1 entry, for loop to print all messages
+        # because there might be more than 1 entry, for loop to print all messages
         responseMessagesDate = []
         str = ""
         for m in db.journalMessages.find(key):
             responseMessagesDate.append(m['message'])
-    
+
         for msgDate in responseMessagesDate:
             str += msgDate + '\n\n'\
-            
-        #print (m['message']) debugging
+
+        # print (m['message']) debugging
     except:
         await ctx.respond('The correct date syntax is dd-mm-YYYY!', ephemeral=True)
-    
+
     if str != "":
         await ctx.respond('Your journal entries  for ' + date + ' are:\n\n' + str, ephemeral=True)
     else:
-         await ctx.respond('You haven\'t journaled this day!', ephemeral=True)
+        await ctx.respond('You haven\'t journaled this day!', ephemeral=True)
 
-#error handler for cooldown
+# error handler for cooldown
+
+
 @bot.event
 async def on_application_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
@@ -143,5 +154,5 @@ async def on_application_command_error(ctx, error):
         raise error
 
 
-#start the bot with the token
+# start the bot with the token
 bot.run(token)
